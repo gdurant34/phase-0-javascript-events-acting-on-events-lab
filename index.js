335f381f00bff69471a2b8c57a6ff388411607ae
Grace Durant <gdurant34@gmail.com>
2022-09-20 16:03:00 -0700

    commit tree d26c30a919e83e6f6270b93f638d2f5f20d468d4

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
+  
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   });
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers,10);
+
+    if(left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    };
+};
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers,10);
+
+    if(left <= 380) {
+        dodger.style.left = `${left + 1}px`;
+    };
+};
+
+document.addEventListener("keydown", function(e) {
+    if(e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    };
+});
+
+document.addEventListener("keydown", function(e) {
+    if(e.key === "ArrowRight") {
+        moveDodgerRight();
+    } else if(e.key === "ArroeRight") {
+        moveDodgerRight();
+    };
+});
+
